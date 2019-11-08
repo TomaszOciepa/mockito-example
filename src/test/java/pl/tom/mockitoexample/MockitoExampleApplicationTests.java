5ec6613e5df082629d9f9b8bd0c9ad5dafdcbf67
tomaszociepa <tom.ociepa@gmail.com>
2019-11-08 14:15:36 +0100

    first commit

---
+++ src/test/java/pl/tom/mockitoexample/MockitoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tom.mockitoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
